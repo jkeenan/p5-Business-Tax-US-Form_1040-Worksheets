9b0a761ee8e1abb621687972639d486a942cbfe1
James E Keenan <jkeenan@cpan.org>
2024-02-23 20:21:40 -0500

    version 0.04
    Introduce sub pp_ssbw to pretty-print the SSBW; document; test.

--- lib/Business/Tax/US/Form_1040/Worksheets.pm
+++ lib/Business/Tax/US/Form_1040/Worksheets.pm
@@ -8,7 +8,7 @@ BEGIN {
     use Exporter ();
     use List::Util qw( min );
     our ($VERSION, @ISA, @EXPORT_OK);
-    $VERSION     = '0.03';
+    $VERSION     = '0.04';
     @ISA         = qw(Exporter);
     @EXPORT_OK   = qw(
         social_security_benefits
