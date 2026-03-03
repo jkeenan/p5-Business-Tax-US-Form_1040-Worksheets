af0dd51608730cdb8790825bee72a6d481cde0d5
James E Keenan <jkeenan@cpan.org>
2026-03-03 06:55:54 -0500

    CPAN version 0.08
    Updates for tax year 2025.

--- lib/Business/Tax/US/Form_1040/Worksheets.pm
+++ lib/Business/Tax/US/Form_1040/Worksheets.pm
@@ -8,7 +8,7 @@ BEGIN {
     use Exporter ();
     use List::Util qw( min );
     our ($VERSION, @ISA, @EXPORT_OK);
-    $VERSION     = '0.07_001';
+    $VERSION     = '0.08';
     @ISA         = qw(Exporter);
     @EXPORT_OK   = qw(
         social_security_benefits
